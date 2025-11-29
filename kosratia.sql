8a6247cb59947ce238ba9ea3130606a258ead923
aulyakz <aulyakhiranazas@gmail.com>
2025-11-29 23:09:50 +0700

    file databasenya

---
+++ kosratia.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 29 Nov 2025 pada 17.02
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kosratia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pemilik`
+--
+
+CREATE TABLE `pemilik` (
+  `username` varchar(20) NOT NULL,
+  `password` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pemilik`
+--
+
+INSERT INTO `pemilik` (`username`, `password`) VALUES
+('admin', 123),
+('maman', 123456);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penghuni`
+--
+
+CREATE TABLE `penghuni` (
+  `nokamar` int(10) NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `nohp` int(20) NOT NULL,
+  `durasi` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `penghuni`
+--
+
+INSERT INTO `penghuni` (`nokamar`, `status`, `nama`, `alamat`, `nohp`, `durasi`) VALUES
+(1, 'terisi', 'aulya', 'padang pariaman', 12345678, 90);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `pemilik`
+--
+ALTER TABLE `pemilik`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indeks untuk tabel `penghuni`
+--
+ALTER TABLE `penghuni`
+  ADD PRIMARY KEY (`nokamar`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
